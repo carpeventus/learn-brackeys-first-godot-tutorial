792e5060d6cbc864640aadaa9c858a0a031c0de5
ash1ta <haiyu19931121@163.com>
2024-07-20 23:18:33 +0800

    update

---
+++ dead_area.gd
@@ -0,0 +1,17 @@
+extends Area2D
+
+@onready var timer: Timer = $Timer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	body_entered.connect(_on_body_entered)
+	timer.timeout.connect(_on_timer_timeout)
+
+func _on_timer_timeout() -> void:
+	get_tree().reload_current_scene()
+
+
+func _on_body_entered(_body: Node2D) -> void:
+	print("You are dead!")
+	timer.start()
